f93ebd96b560139e0ba45b85a5bf75498776e56a
nicemayi <wangzhe.dut@gmail.com>
2017-02-06 18:32:38 -0800

    init commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+// Main starting point of the application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan  = require('morgan');
+const mongoose = require('mongoose');
+
+// DB setup
+mongoose.connect('mongodb://localhost:auth/auth');
+
+const app = express();
+const router = require('./router');
+// App Setup
+// morgan is a login framework (mostly for debugging)
+// bodyParser parse to json
+app.use(morgan('combined'));
+app.use(bodyParser.json({
+  type: '*/*'
+}));
+router(app);
+
+// Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log(`Server listening on : ${port}`);

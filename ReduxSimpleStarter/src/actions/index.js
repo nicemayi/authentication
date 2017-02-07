ca0c204c4a8052618eb63ba68f40ad104762efb8
Game <Game>
2017-02-06 22:35:26 -0800

    add client side

--- ReduxSimpleStarter/src/actions/index.js
+++ ReduxSimpleStarter/src/actions/index.js
@@ -1,11 +1,19 @@
+import axios from 'axios';
+const ROOT_URL = 'http://localhost:3090';
+
 export function signinUser({email, password}) {
-  // Submit email/password to the server
+  console.log("here");
+  return function(dispatch) {
+    console.log("in here, dispatch", dispatch);
+    axios.post(`${ROOT_URL}/signin`, {email, password});
+    // Submit email/password to the server
 
-  // If request is good...
-  // - Update state to indicate user is authenticated
-  // - Save the JWT token
-  // - redirect to the route './feature'
+    // If request is good...
+    // - Update state to indicate user is authenticated
+    // - Save the JWT token
+    // - redirect to the route './feature'
 
-  // If request is bad...
-  // - Show an error to the user
+    // If request is bad...
+    // - Show an error to the user
+  }
 }
--- ReduxSimpleStarter/src/components/auth/Signin.js
+++ ReduxSimpleStarter/src/components/auth/Signin.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
 import {reduxForm} from 'redux-form';
+import * as actions from '../../actions'
 
 class Signin extends Component {
   handleFormSubmit({email, password}) {
     console.log(email, password);
     // Need to do something to log user in
+    console.log(this.props);
+    this.props.signinUser({email, password});
   }
 
   render() {
@@ -28,4 +31,4 @@ class Signin extends Component {
 export default reduxForm({
   form: 'signin',
   fields: ['email', 'password']
-})(Signin);
+}, null, actions)(Signin);

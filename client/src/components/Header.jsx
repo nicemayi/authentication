866e726ab856515ad1dcb42ed7d5fef95368fb2a
nicemayi <wangzhe.dut@gmail.com>
2017-02-07 12:47:47 -0800

    wrap up

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,14 +5,12 @@ import {Link} from 'react-router';
 class Header extends Component {
   renderLinks() {
     if (this.props.authenticated) {
-      // show a link to sign out
       return (
         <li className="nav-item">
           <Link className="nav-link" to="/signout">Sign Out</Link>
         </li>
       );
     } else {
-      // show a link to sign in or sign up
       return [
         <li className="nav-item" key={1}><Link className="nav-link" to="/signin">Sign In</Link></li>,
         <li className="nav-item" key={2}><Link className="nav-link" to="/signup">Sign Up</Link></li>
--- client/src/index.js
+++ client/src/index.js
@@ -20,9 +20,7 @@ const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 const store = createStoreWithMiddleware(reducers)
 
 const token = localStorage.getItem("token");
-// If we have a token, consider the user to be signed in.
 if (token) {
-  // we need to update application state
   store.dispatch({type: AUTH_USER});
 }
 

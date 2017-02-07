53043ba4f9025c4f1df0186418ba8cc376a25ee7
Game <Game>
2017-02-07 08:53:33 -0800

    107

--- ReduxSimpleStarter/src/actions/index.js
+++ ReduxSimpleStarter/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import {browserHistory} from 'react-router';
-import {AUTH_USER} from './types'
+import {AUTH_USER, AUTH_ERROR, UNAUTH_USER} from './types'
 const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({email, password}) {
@@ -12,13 +12,26 @@ export function signinUser({email, password}) {
         // - Update state to indicate user is authenticated
         dispatch({type: AUTH_USER});
         // - Save the JWT token
-
+        localStorage.setItem('token', response.data.token);
         // - redirect to the route './feature'
         browserHistory.push('/feature')
       })
       .catch(() => {
         // If request is bad...
         // - Show an error to the user
+        dispatch(authError('Bad Login Info'));
       });
   }
+}
+
+export function authError(error) {
+  return {
+    type: AUTH_ERROR,
+    payload: error
+  }
+}
+
+export function signoutUser() {
+  localStorage.removeItem('token');
+  return {type: UNAUTH_USER};
 }
--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -8,6 +8,8 @@ import reduxThunk from 'redux-thunk';
 
 import App from './components/app';
 import Signin from './components/auth/Signin';
+import Signup from './components/auth/Signup';
+import Signout from './components/auth/Signout';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
@@ -17,6 +19,8 @@ ReactDOM.render(
     <Router history={browserHistory}>
       <Route path='/' component={App}>
         <Route path="signin" component={Signin}></Route>
+        <Route path='signup' component={Signup}></Route>
+        <Route path='signout' component={Signout}></Route>
       </Route>
     </Router>
   </Provider>
